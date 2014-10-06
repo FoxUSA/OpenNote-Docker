0c98e2b9b17839993bccce4b5c6956385054764b
FoxUSA <jake@jakelis.com>
2014-10-05 22:28:35 -0700

    Updated release package

--- UpgradeScripts/14.07.02.sh
+++ UpgradeScripts/14.07.02.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/FoxUSA/OpenNote/releases/download/14.07.01/OpenNote.zip -P /app
+wget https://github.com/FoxUSA/OpenNote/releases/download/14.07.02/OpenNote.zip -P /app
 
 #Unpack the new version except config and install files
 unzip -o OpenNote.zip -d /app -x *openNote.config.js *Service/Config.* *Service/install.php
