ffc02bd9e7230b815516b0419c9f5326e36f0c96
FoxUSA <jake@jakelis.com>
2014-10-05 22:25:47 -0700

    Upgrade script

---
+++ UpgradeScripts/14.07.02.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget https://github.com/FoxUSA/OpenNote/releases/download/14.07.01/OpenNote.zip -P /app
+
+#Unpack the new version except config and install files
+unzip -o OpenNote.zip -d /app -x *openNote.config.js *Service/Config.* *Service/install.php
+
+#Re permission
+chmod 755 /app -R
+chown www-data:www-data /app -R
+chmod 755 /*.sh
+
+#Remove OpenNote Zip
+rm /app/OpenNote.zip
